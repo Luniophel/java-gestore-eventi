53aeecd7f6c66a759abfd7ac7a39bbc7afc4eff9
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-07-01 12:37:59 +0200

    MILESTONE 1

--- src/jana60/Evento.java
+++ src/jana60/Evento.java
@@ -1,17 +1,21 @@
 package jana60;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Evento {
+	
+	
 	
 	//ATTRIBUTI
 	private String titolo;
 	private LocalDateTime data;
 	private int nPostiTotali, nPostiPrenotati;
 	
-	private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("gg/MM/yyyy, Ore: HH:mm");
+	//Formattazione della data + ora dell'evento
+	private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy, HH:mm");
+	
+	
 	
 	//COSTRUTTORI
 	public Evento(String titolo, LocalDateTime data, int nPostiTotali) throws Exception
@@ -29,13 +33,8 @@ public class Evento {
 		}
 		catch (IllegalArgumentException iae)
 		{
-				datiValidi = false;
-				mexErrore += " " + iae.getMessage();
-		}
-		catch (Exception e)
-		{
 			datiValidi = false;
-			mexErrore += " " + e.getMessage();
+			mexErrore += " " + iae.getMessage();
 		}
 		
 		
@@ -51,24 +50,97 @@ public class Evento {
 		}
 	}
 	
-	//GETTER / SETTER
 	
-	//METODI
-	private void validaData(LocalDateTime data) throws Exception
-	{
-		if (data.isBefore(LocalDateTime.now()))
-			throw new Exception("L'evento si è già svolto in data:\n" + dtf.format(data));
+	
+	//GETTER / SETTER
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public int getnPostiTotali() {
+		return nPostiTotali;
+	}
+
+	public int getnPostiPrenotati() {
+		return nPostiPrenotati;
 	}
 	
+	
+	
+	//METODI
+	public void prenota() throws Exception
+	{
+		try
+		{
+			validaData(data);		
+		}
+		catch (IllegalArgumentException iae)
+		{
+			throw new Exception("L'evento è già passato.");	
+		}
+		
+		if (nPostiPrenotati == nPostiTotali) //Confronto con == in quanto, per come strutturato, non potrà mai superare il valore massimo.
+			throw new Exception("Non ci sono più posti.");
+		else
+			this.nPostiPrenotati++;
+		
+	}
+	
+	public void disdici() throws Exception
+	{
+		try
+		{
+			validaData(data);		
+		}
+		catch (IllegalArgumentException iae)
+		{
+			throw new Exception("L'evento è già passato.");	
+		}
+		
+		if (nPostiPrenotati == 0) //Confronto con == in quanto, per come strutturato, non potrà mai superare il valore minimo.
+			throw new Exception("Non ci sono prenotazioni al momento.");
+		else
+			this.nPostiPrenotati--;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return dtf.format(data) + " - " + titolo;
+	}
+	
+	
+	
+	//VALIDATORI
+	//Verifica che la data inserita non sia precedente alla data di oggi
+	private void validaData(LocalDateTime data) throws IllegalArgumentException
+	{
+		if (data.isBefore(LocalDateTime.now()))
+			throw new IllegalArgumentException("Non è possibile inserire date precedenti all'odierna.");
+	}
+	//Verifica che il titolo non sia vuoto
 	private void validaTitolo(String titolo) throws IllegalArgumentException
 	{
 		if (titolo.isBlank())
 			throw new IllegalArgumentException("Non puoi lasciare vuoto il titolo dell'evento.");
 	}
-	
+	//Verifica che i posti totali siano maggiori di zero
 	private void validaPostiTotali(int nPostiTotali) throws IllegalArgumentException
 	{
 		if (nPostiTotali <= 0)
 			throw new IllegalArgumentException("Non puoi inserire 0 o valori negativi.");	
 	}
+
 }
